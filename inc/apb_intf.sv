9f6eace8cfad5276ca65f12f53ceccea26b02422
Luca Hanel <luca.hanel@hensoldt-cyber.com>
2020-08-27 17:35:30 +0200

    bugfix

--- inc/apb_intf.sv
+++ inc/apb_intf.sv
@@ -16,16 +16,16 @@ interface apb_bus_t #(
   logic                   PWRITE;     // Write enable (low=read)
   logic [DATA_WIDTH-1:0]  PWDATA;     // Data to be written (master->slave)
   logic                   PREADY;     // Slave is ready
-  logic                   PRDATA;     // Data read (slave->master)
+  logic [DATA_WIDTH-1:0]  PRDATA;     // Data read (slave->master)
   logic                   PSLVERR;    // Slave encountered error. If not used, tie to low
 
   modport master (
       input PREADY, PRDATA, PSLVERR,
-      output PCLK, PRESETn, PADDR, PSELx, PENABLE, PWRITE, PWDATA
+      output PCLK, PRESETn, PADDR, PSEL, PENABLE, PWRITE, PWDATA
   );
 
   modport slave (
-      input PCLK, PRESETn, PADDR, PSELx, PENABLE, PWRITE, PWDATA,
+      input PCLK, PRESETn, PADDR, PSEL, PENABLE, PWRITE, PWDATA,
       output PREADY, PRDATA, PSLVERR
   );
 endinterface //apb_bus_t
--- src/apb_bar.sv
+++ src/apb_bar.sv
@@ -31,7 +31,7 @@
 module apb_bar#(
   parameter                             APB_DATA_WIDTH  = 32,
   parameter                             APB_ADDR_WIDTH  = 32,
-  parameter                             APB_ACT_ADDR_W  = 10,
+  parameter                             APB_ACT_ADDR_W  = 12,
   parameter                             N_SLAVES
 )(
   apb_bus_t.slave           slave_port,
@@ -41,38 +41,52 @@ module apb_bar#(
 logic [APB_ADDR_WIDTH-1:0]  PADDR;
 logic [APB_DATA_WIDTH-1:0]  PRDATA;
 logic                       PREADY;
+logic                       PSLVERR;
 logic [N_SLAVES-1:0]        PSEL;
 
-logic [$clog2(N_SLAVE)-1:0] slave_sel;
+logic [APB_DATA_WIDTH-1:0]  PRDATA_i[N_SLAVES];
+logic                       PREADY_i[N_SLAVES];
+logic                       PSLVERR_i[N_SLAVES];
+logic [$clog2(N_SLAVES)-1:0] slave_sel;
 
 // Assign the signals to the slaves
 for(genvar ii = 0; ii < N_SLAVES; ii = ii + 1) begin
   assign master_port[ii].PCLK     = slave_port.PCLK;
   assign master_port[ii].PRESETn  = slave_port.PRESETn;
+  assign master_port[ii].PSEL     = PSEL[ii];
   assign master_port[ii].PADDR    = PADDR;
+  assign master_port[ii].PWRITE   = slave_port.PWRITE;
   assign master_port[ii].PWDATA   = slave_port.PWDATA;
   assign master_port[ii].PENABLE  = slave_port.PENABLE;
+  assign PRDATA_i[ii] = master_port[ii].PRDATA;
+  assign PREADY_i[ii] = master_port[ii].PREADY;
+  assign PSLVERR_i[ii] = master_port[ii].PSLVERR;
 end
 
 // Assign the signals to the master
 assign slave_port.PRDATA  = PRDATA;
 assign slave_port.PREADY  = PREADY;
-assign slave_port.PSLVERR = PSLVERR
+assign slave_port.PSLVERR = PSLVERR;
 
 // slave select
-assign slave_sel = slave_port[APB_ACT_ADDR_W+$clog2(N_SLAVES)-1:APB_ACT_ADDR_W];
+assign slave_sel = slave_port.PADDR[APB_ACT_ADDR_W+$clog2(N_SLAVES)-1:APB_ACT_ADDR_W];
 
 // slave arbiter
 always_comb
 begin
   PSEL    = 'b0;
   PADDR   = 'b0;
+  PRDATA  = 'b0;
+  PREADY  = 'b0;
+  PSLVERR = 'b0;
 
-  PSEL[slave_sel]           = 1'b1;
-  PRDATA                    = master_port[slave_sel].PRDATA;
-  PREADY                    = master_port[slave_sel].PREADY;
-  PSLVERR                   = master_port[slave_sel].PSLVERR;
-  PADDR[APB_ACT_ADDR_W-1:0] = slave_port[APB_ACT_ADDR_W-1:0];
+  if(slave_port.PSEL) begin
+    PSEL[slave_sel]           = 1'b1;
+    PRDATA                    = PRDATA_i[slave_sel];
+    PREADY                    = PREADY_i[slave_sel];
+    PSLVERR                   = PSLVERR_i[slave_sel];
+    PADDR[APB_ACT_ADDR_W-1:0] = slave_port.PADDR[APB_ACT_ADDR_W-1:0];
+  end
 end
 
 endmodule
