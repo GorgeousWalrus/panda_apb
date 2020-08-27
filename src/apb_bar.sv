9d7f86b17028a19d7e1b68a429fe5f692504ccf0
Luca Hanel <luca.hanel@hensoldt-cyber.com>
2020-08-27 13:34:23 +0200

    initial commit

---
+++ src/apb_bar.sv
@@ -0,0 +1,27 @@
+// ------------------------ Disclaimer -----------------------
+// No warranty of correctness, synthesizability or 
+// functionality of this code is given.
+// Use this code under your own risk.
+// When using this code, copy this disclaimer at the top of 
+// Your file
+//
+// (c) Luca Hanel 2020
+//
+// ------------------------------------------------------------
+//
+// Module name: apb_bar
+// 
+// Functionality: Interconnect for APB bus
+//
+// Tests: 
+//
+// ------------------------------------------------------------
+
+module apb_bar#(
+  parameter APB_DATA_WIDTH = 32,
+  parameter APB_ADDR_WIDTH = 32
+)(
+
+);
+
+endmodule
